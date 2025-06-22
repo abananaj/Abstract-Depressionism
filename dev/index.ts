dfbec9d9c7380d50ab7c927ae6079cd4d6625bde
abananaj <anna@theatrum.dev>
2025-06-21 22:38:21 -0700

    initial commit

---
+++ dev/index.ts
@@ -0,0 +1,20 @@
+// ==================== TESTS
+console.log('hello from index.js');
+// module
+import { testModule } from "./scripts/ts/test-module.js";
+console.log(testModule);
+// babel
+[1, 2, 3].map(n => n + 1);
+
+
+// ==================== STYLES
+import "./index.scss";
+
+// ==================== NPM IMPORTS
+import "gsap";
+import "bootstrap";
+
+// ==================== CUSTOM MODULES
+import './scripts/js/header.js';
+import './scripts/js/tiltgrid.js';
+// ==================== CUSTOM MODULES
